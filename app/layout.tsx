7f2775408d0574705dcab6cf2ee4d321451e652b
phareedahadamu <phareedahadamu@gmail.com>
2025-02-21 14:08:39 +0100

    testing tokens

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+// import Head from "next/head";
 
 const monty = Montserrat({
   variable: "--font-montserrat",
@@ -20,25 +20,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Origin Trial Token for AI LanguageDetector */}
-        <meta
-          httpEquiv="Origin-Trial"
-          content="AkhtP5xAdMPJDPlhHQpSN2EpKRUETOULCsuKULX9sZoGjBYQwUG9KGUN81wte8t9wOIytRh1bm7of1fPsI1KmQoAAACReyJvcmlnaW4iOiJodHRwczovL3BoYXJlZWRhaGFkYW11LXN1bW1hcml6ZXItdHJhbnNsYXRvci5uZXRsaWZ5LmFwcDo0NDMiLCJmZWF0dXJlIjoiTGFuZ3VhZ2VEZXRlY3Rpb25BUEkiLCJleHBpcnkiOjE3NDk1OTk5OTksImlzU3ViZG9tYWluIjp0cnVlfQ=="
-        />
+      {/* Origin Trial Token for AI LanguageDetector */}
+      <meta
+        httpEquiv="Origin-Trial"
+        content="AkhtP5xAdMPJDPlhHQpSN2EpKRUETOULCsuKULX9sZoGjBYQwUG9KGUN81wte8t9wOIytRh1bm7of1fPsI1KmQoAAACReyJvcmlnaW4iOiJodHRwczovL3BoYXJlZWRhaGFkYW11LXN1bW1hcml6ZXItdHJhbnNsYXRvci5uZXRsaWZ5LmFwcDo0NDMiLCJmZWF0dXJlIjoiTGFuZ3VhZ2VEZXRlY3Rpb25BUEkiLCJleHBpcnkiOjE3NDk1OTk5OTksImlzU3ViZG9tYWluIjp0cnVlfQ=="
+      />
 
-        {/* Origin Trial Token for AI Translator */}
-        <meta
-          httpEquiv="Origin-Trial"
-          content="AuTbatY1CvPpcQgXcxNti0e7jCk/4ryzA9ldCX6EPKQK9I95mu2WE50+lyvuzmBVhSUSOrr0QmcltZUWDSKXKggAAACLeyJvcmlnaW4iOiJodHRwczovL3BoYXJlZWRhaGFkYW11LXN1bW1hcml6ZXItdHJhbnNsYXRvci5uZXRsaWZ5LmFwcDo0NDMiLCJmZWF0dXJlIjoiVHJhbnNsYXRpb25BUEkiLCJleHBpcnkiOjE3NTMxNDI0MDAsImlzU3ViZG9tYWluIjp0cnVlfQ=="
-        />
+      {/* Origin Trial Token for AI Translator */}
+      <meta
+        httpEquiv="Origin-Trial"
+        content="AuTbatY1CvPpcQgXcxNti0e7jCk/4ryzA9ldCX6EPKQK9I95mu2WE50+lyvuzmBVhSUSOrr0QmcltZUWDSKXKggAAACLeyJvcmlnaW4iOiJodHRwczovL3BoYXJlZWRhaGFkYW11LXN1bW1hcml6ZXItdHJhbnNsYXRvci5uZXRsaWZ5LmFwcDo0NDMiLCJmZWF0dXJlIjoiVHJhbnNsYXRpb25BUEkiLCJleHBpcnkiOjE3NTMxNDI0MDAsImlzU3ViZG9tYWluIjp0cnVlfQ=="
+      />
 
-        {/* Origin Trial Token for AI Summarizer */}
-        <meta
-          httpEquiv="Origin-Trial"
-          content="AvENn/7//IswDtswsFct8KsuCoNJKA+3VPjbiFLMTz/VQhrkaREDN/Cas1zV/pnICbl2fhnnV31UcPWur+ijbwUAAACPeyJvcmlnaW4iOiJodHRwczovL3BoYXJlZWRhaGFkYW11LXN1bW1hcml6ZXItdHJhbnNsYXRvci5uZXRsaWZ5LmFwcDo0NDMiLCJmZWF0dXJlIjoiQUlTdW1tYXJpemF0aW9uQVBJIiwiZXhwaXJ5IjoxNzUzMTQyNDAwLCJpc1N1YmRvbWFpbiI6dHJ1ZX0="
-        />
-      </Head>
+      {/* Origin Trial Token for AI Summarizer */}
+      <meta
+        httpEquiv="Origin-Trial"
+        content="AvENn/7//IswDtswsFct8KsuCoNJKA+3VPjbiFLMTz/VQhrkaREDN/Cas1zV/pnICbl2fhnnV31UcPWur+ijbwUAAACPeyJvcmlnaW4iOiJodHRwczovL3BoYXJlZWRhaGFkYW11LXN1bW1hcml6ZXItdHJhbnNsYXRvci5uZXRsaWZ5LmFwcDo0NDMiLCJmZWF0dXJlIjoiQUlTdW1tYXJpemF0aW9uQVBJIiwiZXhwaXJ5IjoxNzUzMTQyNDAwLCJpc1N1YmRvbWFpbiI6dHJ1ZX0="
+      />
       <body
         className={`${monty.variable} antialiased px-[24px] h-[100dvh] flex justify-center items-start w-[100%]`}
       >
